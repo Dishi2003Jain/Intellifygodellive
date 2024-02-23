3bc16e691249727af1863b782b350a1e570e346c
Dishi <dishinayak223@gmail.com>
2024-02-23 10:50:32 +0530

    mongo_url

--- pages/topicintroduction.js
+++ pages/topicintroduction.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { FaArrowLeft } from "react-icons/fa6";
 import { FaArrowRight } from "react-icons/fa6";
 import styles from "../styles/Home.module.css";
+
 const topicintroduction = () => {
   return (
     <div className={styles.uicontainer}>
